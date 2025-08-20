1fd2f03728d74b72749220774fdfd7947a98ee49
Guillaume PÉVRIER <guillaumepevrier@gmail.com>
2025-08-20 19:09:40 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -3,11 +3,9 @@ import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { type NextRequest, NextResponse } from 'next/server'
 
 export async function updateSession(request: NextRequest) {
-  let response = NextResponse.next({
-    request: {
-      headers: request.headers,
-    },
-  })
+  let supabaseResponse = NextResponse.next({
+    request,
+  });
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -18,34 +16,14 @@ export async function updateSession(request: NextRequest) {
           return request.cookies.get(name)?.value
         },
         set(name: string, value: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value,
-            ...options,
-          })
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          })
-          response.cookies.set({
+          supabaseResponse.cookies.set({
             name,
             value,
             ...options,
           })
         },
         remove(name: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value: '',
-            ...options,
-          })
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          })
-          response.cookies.set({
+          supabaseResponse.cookies.set({
             name,
             value: '',
             ...options,
@@ -53,9 +31,10 @@ export async function updateSession(request: NextRequest) {
         },
       },
     }
-  )
+  );
 
-  await supabase.auth.getUser()
+  // Refresh expired Supabase auth session
+  await supabase.auth.getUser();
 
-  return response
+  return supabaseResponse
 }
