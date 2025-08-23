f18344a25321958dbda8979a0475e8cd144cb58b
Guillaume PÉVRIER <guillaumepevrier@gmail.com>
2025-08-23 16:17:59 +0000

    test des notifications

--- supabase/migrations/20240729210000_create_match_update_trigger.sql
+++ supabase/migrations/20240729210000_create_match_update_trigger.sql
@@ -2,7 +2,7 @@
 --  de données.
 -- Il est exécuté automatiquement par Supabase lorsque vous déployez vos migrations.
 -- IMPORTANT : Ce trigger nécessite que l'extension `http` soit activée.
---`create extension if not exists http ith schema extensions;` dans l'éditeur SQL de Supabase avant d'appliquer cette migration.
+--`crth schema extensions;` dans l'éditeur SQL de Supabase avant d'appliquer cette migration.
 
 -- 1. Définition de la fonction qui sera appelée par le trigger.
 create or replace function handle_match_update_and_notify()
