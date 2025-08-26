ad79ba2d713ce443b6d6393dc03f713fa1532930
Guillaume PÉVRIER <guillaumepevrier@gmail.com>
2025-08-26 09:17:57 +0000

    Analyse mon application Next.js actuelle, qui est un manager de gestion

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,22 @@ export default function RootLayout({
   return (
     <html lang="fr" suppressHydrationWarning>
       <head>
+        <meta name="application-name" content="NBFC Futsal Manager" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="NBFC Manager" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-TileColor" content="#fd5156" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#01182a" />
+        
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.json" />
+
         <title>NBFC Futsal Manager</title>
         <meta name="description" content="Application de gestion de tactique et de match pour le NBFC Futsal." />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -20,7 +36,6 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Black+Ops+One&display=swap" rel="stylesheet" />
-        <link rel="manifest" href="/manifest.json" />
       </head>
       <body className="font-body antialiased min-h-screen">
         <ThemeProvider
